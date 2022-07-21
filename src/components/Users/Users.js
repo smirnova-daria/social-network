2d6da6296f073f6a203f37f0594be4ee31e6ee76
Daria Smirnova <darya.smirn1@yandex.ru>
2022-07-21 20:00:31 +0300

    Рефакторинг: переписана компонента Users на классовую компоненту (т.к. функциональная компонента не могла быть чистой из-за запроса к api)

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -2,24 +2,26 @@ import React from "react";
 import User from "./User/User";
 import * as axios from "axios";
 
-const Users = (props) => {
-  if (props.users.length === 0) {
+class Users extends React.Component {
+  constructor(props) {
+    super(props);
     axios
       .get("https://social-network.samuraijs.com/api/1.0/users")
       .then((res) => props.setUsers(res.data.items));
   }
-
-  return (
-    <div>
-      {props.users.map((u) => (
-        <User
-          key={u.id}
-          user={u}
-          unfollow={props.unfollow}
-          follow={props.follow}
-        />
-      ))}
-    </div>
-  );
-};
+  render() {
+    return (
+      <div>
+        {this.props.users.map((u) => (
+          <User
+            key={u.id}
+            user={u}
+            unfollow={this.props.unfollow}
+            follow={this.props.follow}
+          />
+        ))}
+      </div>
+    );
+  }
+}
 export default Users;
