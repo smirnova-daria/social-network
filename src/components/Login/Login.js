61f4cc0fa30f8e4b9485f4f8bfb1f3772eec4f7a
Daria Smirnova <darya.smirn1@yandex.ru>
2022-07-26 18:47:24 +0300

    Компонент Login: добавлена форма регистрации

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,7 +1,54 @@
 import React from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import loginFormSchema from "../FormValidation/LoginFormSchema";
 
-const Login = (props) => {
-  return <h2>LOGIN</h2>;
-};
+const Login = () => (
+  <div>
+    <h1>Login</h1>
+    <Formik
+      initialValues={{ email: "", password: "", rememberMe: false }}
+      validate={(values) => {
+        const errors = {};
+        if (!values.email) {
+          errors.email = "Required";
+        } else if (
+          !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+        ) {
+          errors.email = "Invalid email address";
+        }
+        return errors;
+      }}
+      onSubmit={(values) => {
+        console.log(values);
+      }}
+      validationSchema={loginFormSchema}
+    >
+      {() => (
+        <Form>
+          <div>
+            <Field type={"text"} name={"email"} placeholder={"e-mail"} />
+          </div>
+          <ErrorMessage name="email" component="div" />
+
+          <div>
+            <Field
+              type={"password"}
+              name={"password"}
+              placeholder={"password"}
+            />
+          </div>
+          <ErrorMessage name="password" component="div" />
+
+          <div>
+            <Field type={"checkbox"} name={"rememberMe"} />
+            <label htmlFor={"rememberMe"}> remember me </label>
+          </div>
+
+          <button type={"submit"}>Log in</button>
+        </Form>
+      )}
+    </Formik>
+  </div>
+);
 
 export default Login;
