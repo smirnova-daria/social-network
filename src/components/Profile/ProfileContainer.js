a5dc6b80d3ade06e2f1b4056a0790cfcd6639d81
Daria Smirnova <darya.smirn1@yandex.ru>
2022-07-28 10:45:00 +0300

    Исправлен баг: неавторизованный пользователь не мог посмотреть других пользователей, т.к. происходил редирект

--- src/components/Profile/ProfileContainer.js
+++ src/components/Profile/ProfileContainer.js
@@ -6,23 +6,33 @@ import {
   getStatus,
   updateStatus,
 } from "../../redux/profile-reducer";
-import { useLocation, useNavigate, useParams } from "react-router-dom";
-import { withAuthRedirect } from "../../hoc/withAuthRedirect";
+import {
+  Navigate,
+  useLocation,
+  useNavigate,
+  useParams,
+} from "react-router-dom";
 import { compose } from "redux";
 
 class ProfileContainer extends React.Component {
+  state = {
+    redirect: false,
+  };
   componentDidMount() {
     let userId = this.props.router.params.userId;
     if (!userId) {
       userId = this.props.authorizedUserId;
       if (!userId) {
-        this.props.history.push("/login");
+        this.setState({ redirect: true });
       }
     }
-    this.props.getProfile(userId);
-    this.props.getStatus(userId);
+    if (userId) {
+      this.props.getProfile(userId);
+      this.props.getStatus(userId);
+    }
   }
   render() {
+    if (this.state.redirect) return <Navigate to={"/login"} />;
     return <Profile {...this.props} />;
   }
 }
@@ -31,6 +41,7 @@ const mapStateToProps = (state) => ({
   profile: state.profilePage.profile,
   status: state.profilePage.status,
   authorizedUserId: state.auth.id,
+  isAuth: state.auth.isAuth,
 });
 
 const withRouter = (Component) => {
@@ -50,6 +61,5 @@ export default compose(
     getStatus,
     updateStatus,
   }),
-  withRouter,
-  withAuthRedirect
+  withRouter
 )(ProfileContainer);
