8c8f94bd7d275fa0ff64d234d5750b9a58be5a14
Daria Smirnova <darya.smirn1@yandex.ru>
2022-07-28 15:41:38 +0300

    Использование хука useEffect для корректного отображения статуса на странице профиля

--- src/components/Profile/ProfileInfo/ProfileStatus/ProfileStatusWithHooks.js
+++ src/components/Profile/ProfileInfo/ProfileStatus/ProfileStatusWithHooks.js
@@ -1,9 +1,13 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import s from "./ProfileStatus.module.css";
 
 const ProfileStatus = (props) => {
   const [editMode, setEditMode] = useState(false);
   const [status, setStatus] = useState(props.status);
+
+  useEffect(() => {
+    setStatus(props.status);
+  }, [props.status]);
 
   const activateEditMode = () => {
     setEditMode(true);
