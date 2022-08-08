325327589773a91623aeb5b31e0140f1e6ae4049
Daria Smirnova <darya.smirn1@yandex.ru>
2022-08-08 14:07:50 +0300

    Рефакторинг типов в файле api: применение generic

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { ProfileType, UserType } from '../types/types';
+import { PhotosType, ProfileType, UserType } from '../types/types';
 
 const axiosSettings = axios.create({
   baseURL: "https://social-network.samuraijs.com/api/1.0/",
@@ -9,11 +9,6 @@ const axiosSettings = axios.create({
   },
 });
 
-type GetUsersType = {
-  items: Array<UserType>
-  totalCount: number,
-  error: string | null
-}
 
 enum ResultCodes {
   Success = 0,
@@ -24,57 +19,49 @@ enum ResultCodeWithCaptcha {
   Captcha = 10
 }
 
-type PutDeleteResponseType = {
-  resultCode: ResultCodes
+type APIResponseType<D = {}, RC = ResultCodes> = {
+  data: D
   messages: Array<string>
-  data: {}
+  resultCode: RC
 }
 
-type AdditionalDataPhotoResponseType = {
-  small: string
-  large: string
+type GetItemsType = {
+  items: Array<UserType>
+  totalCount: number
+  error: string | null
 }
 
-type PhotoPutResponseType = {
-  resultCode: ResultCodes
-  messages: Array<string>
-  data: AdditionalDataPhotoResponseType
+
+type SavePhotoResponseDataType = {
+  photos: PhotosType
 }
 
-type AdditionalDataMeResponseType = {
+type MeResponseDataType = {
   id: number
   email: string
   login: string
 }
-type MeResponseType = {
-  resultCode: ResultCodes
-  messages: Array<string>
-  data: AdditionalDataMeResponseType
-}
-type AdditionalDataLoginResponseType = {
+
+type LoginResponseDataType = {
   userId: number
 }
-type LoginResponseType = {
-  resultCode: ResultCodes | ResultCodeWithCaptcha
-  messages: Array<string>
-  data: AdditionalDataLoginResponseType
-}
+
 type CaptchaResponseType = {
   url: string
 }
 export const usersAPI = {
   getUsers(usersCountOnPage: number, currentPage: number) {
     return axiosSettings
-      .get<GetUsersType>(`users?count=${usersCountOnPage}&page=${currentPage}`)
+      .get<GetItemsType>(`users?count=${usersCountOnPage}&page=${currentPage}`)
       .then((res) => res.data);
   },
 
   unfollowUser(id: number) {
-    return axiosSettings.delete<PutDeleteResponseType>(`follow/${id}`).then((res) => res.data);
+    return axiosSettings.delete(`follow/${id}`).then((res) => res.data);
   },
 
   followUser(id: number) {
-    return axiosSettings.post<PutDeleteResponseType>(`follow/${id}`).then((res) => res.data);
+    return axiosSettings.post<APIResponseType>(`follow/${id}`).then((res) => res.data);
   },
 };
 
@@ -87,16 +74,16 @@ export const profileAPI = {
   },
   updateStatus(status: string) {
     return axiosSettings
-      .put<PutDeleteResponseType>(`profile/status`, {
+      .put<APIResponseType>(`profile/status`, {
         status,
       })
       .then((res) => res.data);
   },
-  savePhoto(file: any) {
+  savePhoto(file: File) {
     const formData = new FormData();
     formData.append("image", file);
     return axiosSettings
-      .put<PhotoPutResponseType>(`profile/photo`, formData, {
+      .put<APIResponseType<SavePhotoResponseDataType>>(`profile/photo`, formData, {
         headers: {
           "Content-Type": "multipart/form-data",
         },
@@ -104,21 +91,21 @@ export const profileAPI = {
       .then((res) => res.data);
   },
   saveProfile(profile: ProfileType) {
-    return axiosSettings.put<PutDeleteResponseType>(`profile`, profile).then((res) => res.data);
+    return axiosSettings.put<APIResponseType>(`profile`, profile).then((res) => res.data);
   },
 };
 
 export const authAPI = {
   getAuthUserData() {
-    return axiosSettings.get<MeResponseType>(`auth/me`).then((res) => res.data);
+    return axiosSettings.get<APIResponseType<MeResponseDataType>>(`auth/me`).then((res) => res.data);
   },
   login(email: string, password: string, rememberMe = false, captcha: string | null = null) {
     return axiosSettings
-      .post<LoginResponseType>(`auth/login`, { email, password, rememberMe, captcha })
+      .post<APIResponseType<LoginResponseDataType, ResultCodes | ResultCodeWithCaptcha>>(`auth/login`, { email, password, rememberMe, captcha })
       .then((res) => res.data);
   },
   logout() {
-    return axiosSettings.delete<PutDeleteResponseType>(`auth/login`).then((res) => res.data);
+    return axiosSettings.delete(`auth/login`).then((res) => res.data);
   },
 };
 
